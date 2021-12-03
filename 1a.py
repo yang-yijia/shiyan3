b17f2f272d81cea064fc7a273ebd6bb3e3abb2fe
Yijia Yang <yangyijia@qq.com>
2021-12-03 13:02:05 +0800

    version 1.0 第一个实验

---
+++ 1a.py
@@ -0,0 +1,23 @@
+import cv2
+from matplotlib import pyplot as plt
+import watermark
+
+img = cv2.imread("gradient.jpg", 0)
+
+# 利用cv2.threshold()支持的5种阈值方式，来实现固定阈值分割
+ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
+
+# 利用matplotlib显示图像
+titles = ['Original', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2, 3, i+1)
+    plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+watermark.addwatermark(plt, "201941404127-杨怡佳")
+plt.show()
